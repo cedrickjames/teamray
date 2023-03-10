c5550c1c1f234dfcc574b08fa742161a060d7022
cedrickjames <cedrickorozo@gmail.com>
2023-03-10 22:06:08 +0800

    teamray

---
+++ nav_properties.php
@@ -0,0 +1,204 @@
+
+
+
+<nav class="bg-blue-900 px-2 sm:px-4 py-2.5 dark:bg-blue-900 fixed w-full z-20 top-0  left-0 border-b border-gray-200 dark:border-gray-600">
+
+<!-- <div class="flex items-center"> -->
+
+
+
+
+
+<style>
+/* Style the buttons */
+.btn {
+ border-radius:18px;
+  border: none;
+  outline: none;
+  padding: 0px 20px;
+  /* background-color: #f1f1f1; */
+  cursor: pointer;
+  font-size: 18px;
+  color: white;
+}
+
+/* Style the active class, and buttons on mouse-over */
+.active, .btn:hover {
+  background-color: #FF8C00;
+  color: white;
+}
+</style>
+
+
+
+
+
+<div class="flex items-center z-40">
+
+   <!-- slidebar button -->
+
+   <!-- <a data-drawer-target="drawer-navigation" data-drawer-show="drawer-navigation" aria-controls="drawer-navigation"> 
+   <i class="fa fa-list fa-xl"  style="color: gray;"></i>
+   </a> -->
+
+
+   
+
+
+<div class="container flex flex-wrap justify-between items-center mx-auto pt-0 pl-4 z-40">
+
+    <a class="flex items-center" data-drawer-target="drawer-navigation" data-drawer-show="drawer-navigation" aria-controls="drawer-navigation">
+        <img src="resources/img/logo/teamlogo - Copy.png" class="mr-3 h-16 sm:h-16" alt="Flowbite Logo" >
+        <!-- <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Website Template1</span> -->
+    </a>
+
+   
+      
+  
+<!-- mobile button expand nav -->
+
+    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+      <span class="sr-only">Open main menu</span>
+      <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+    </button> 
+
+    <!-- <div id="myDIV"> -->
+
+<div class="hidden w-full md:block md:w-auto " id="navbar-default">
+      <ul class="flex flex-col p-4 mt-4 bg-blue-900 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-md md:font-medium md:border-0 md:bg-blue-900 dark:bg-blue-900 md:dark:bg-blue-900 dark:border-blue-900">
+       
+      <li>
+        <a href="tel:0909999999" class="btn"  >Contact Us</a>
+       
+        </li>
+
+      
+      
+      
+      <li>
+
+       
+          <a href="main.php"  class="btn ">Back</a>
+        </li>
+        <li >
+        
+          <!-- <a href="#about" class="btn "  >About</a>
+        </li>
+        <li>
+        <a href="#services" class="btn"  >Services</a>
+        
+        </li> -->
+
+
+        <!-- <li>
+        <a href="product.php" class="btn active"  >Our Properties</a>
+       
+        </li> -->
+
+        <!-- <li>
+        <a href="#contact" class="btn"  >Contact</a>
+         
+        </li> -->
+        
+      </ul>
+    </div> 
+
+    </div>
+
+<!-- 
+      </div> -->
+
+
+    <!-- darkmode button -->
+<!-- 
+<button id="theme-toggle" type="button" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-xs p-1.5">
+    <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
+    <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+</button> -->
+
+  </div>
+
+
+
+
+ 
+
+
+</nav>
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+<footer class="fixed bottom-0 left-0 z-40 p-4 w-full bg-amber-500 border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 bg-white dark:bg-amber-600"style="padding-top: 0.25px;">
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-900">© <?php echo date('Y') ?> <a href="https://teamrayproperties.com/" class="hover:underline">Teamray International</a>: All Rights Reserved
+    </span>
+    
+    <!-- <a href="mailto:j.nemedez@glory.com.ph" class="font-thin text-gray-400 text-left italic font-serif">Powered by: naith.<u class="text-blue-600">ph</u></a> -->
+    
+</footer> 
+
+
+
+
+<script>
+
+</script>
+
+
+
+
+
+<script>
+//   function mouseOver(){
+//    document.getElementById("loginicon").style.display = "inline";
+//    document.getElementById("logintext").style.display = "none";
+
+//   }
+//   function mouseOut(){
+//    document.getElementById("logintext").style.display = "inline";
+//    document.getElementById("loginicon").style.display = "none";
+
+//   }
+
+
+
+
+  // Add active class to the current button (highlight it)
+var header = document.getElementById("navbar-default");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+
+//   var homeoption = document.getElementById("homeoption");
+//   homeoption.classList.remove("text-gray-700");
+//   homeoption.classList.add("text-white");
+//   homeoption.classList.remove("dark:text-gray-400");
+//   homeoption.classList.add("dark:text-white");
+
+
+</script>
+
+
+
+
+
+  
+ 
